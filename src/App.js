1be8f954aefe2d400a7e2725fdf32b19d67a9d23
Ozurumba <ozuruobinna@gmail.com>
2022-07-29 02:55:07 +0100

    fetchdata

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import FetchData from "./components/FetchData.js";
+
+function App() {
+
+  return (
+    <div className='App'>
+      <FetchData></FetchData>
+    </div>
+  );
+}
+
+export default App;
